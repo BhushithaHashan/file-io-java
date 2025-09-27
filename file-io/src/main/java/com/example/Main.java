70f2b30fcb9c1b47474a1368be5216c15b9f861b
BhushithaHashan <bhushithahashan99@gmail.com>
2025-09-27 15:32:23 +0530

    poject init

---
+++ file-io/src/main/java/com/example/Main.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("text.txt");
+        System.out.println(file.getName());
+        System.out.println(file.getAbsolutePath());
+    }
+    
+}
